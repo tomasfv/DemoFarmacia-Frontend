fcbbeb008f0e3cdfefb1c5e3c5fa2577a9c96716
tomasfv <tomasfernandezvaldes@gmail.com>
2025-01-14 08:11:14 -0300

    mod name

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -8,7 +8,7 @@ export default function LandingPage(){
 
   return(
     <Container className="d-flex flex-column justify-content-center align-items-center vh-100 text-center">
-      <h1 className="mb-4">Bienvenidos a Demo App Farmacia</h1>
+      <h1 className="mb-4">Bienvenidos a Farmacia Demo App</h1>
         <Link to="/home">
           <Button className="first-button w-100">ingresar</Button>
         </Link> 
