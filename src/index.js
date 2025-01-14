8b4c4f8cc1536c4bcaca8609c13914a3364f86c2
tomasfv <tomasfernandezvaldes@gmail.com>
2025-01-14 07:51:34 -0300

    deploy frontend

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import store from './redux/store.js';
 import axios from "axios";
 
 
-axios.defaults.baseURL = 'http://localhost:3001';
-//axios.defaults.baseURL = 'URL DEL DEPLOY'; 
+//axios.defaults.baseURL = 'http://localhost:3001';
+axios.defaults.baseURL = 'https://demofarmacia-backend-production.up.railway.app/'; 
 
 
 
