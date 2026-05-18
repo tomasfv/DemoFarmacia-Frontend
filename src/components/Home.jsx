6ed3fd0f1283fd08731adcad05daf400607d382f
tomasfv <tomasfernandezvaldes@gmail.com>
2026-05-18 19:16:47 -0300

    fix table

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ import SearchBar from "./SearchBar";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Table, Button, Container, Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Alert, Col, Row } from 'reactstrap';
 
-export default function Home(){
+export default function Home() {
 
   const dispatch = useDispatch();
   const allClientes = useSelector((state) => state.clientes);
@@ -33,111 +33,125 @@ export default function Home(){
   useEffect(() => {
     dispatch(getClientes());
     const savedPage = localStorage.getItem('currentPage');
-    if(savedPage){
+    if (savedPage) {
       setCurrentPage(parseInt(savedPage, 10))
     }
   }, [dispatch]);
 
-  function handleClick(e){
+  function handleClick(e) {
     e.preventDefault();
     dispatch(getClientes());
     setCurrentPage(1);
     localStorage.removeItem('currentPage');
   };
 
-  function handleSort(e){
+  function handleSort(e) {
     //e.preventDefault();
     dispatch(orderByName(e.target.value));
     setCurrentPage(1);
     setOrden(`Ordenado ${e.target.value}`);
     console.log(orden);
   }
-  
-  function handleDelete(id){
+
+  function handleDelete(id) {
     dispatch(deleteCliente(id));
   }
 
-  return(
-    <Container>
-    <Col sm={10}>
-      <h1>CLIENTES</h1>
-      <SearchBar inHome={true} inObrasSociales={false}/>
-      <Row className="mt-4 mb-4">
-        <Col sm={12} className="d-flex justify-content-between align-items-center flex-wrap">
-          <Link to='/cliente'><Button className='first-button'>crear cliente</Button></Link> {' '}
-        
-        <Dropdown isOpen={dropdownOpen} toggle={toggleDropdown} className="d-inline-block">
-          <DropdownToggle caret className="first-button mt-2"> ordenar </DropdownToggle>
-          <DropdownMenu>
-            <DropdownItem onClick={() => handleSort({ target: { value: 'asc' } })}>
-              Ascendente
-            </DropdownItem>
-            <DropdownItem onClick={() => handleSort({ target: { value: 'desc' } })}>
-              Descendente
-            </DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-        
-        <Paginado
-          itemsPorPagina={clientesPorPagina}
-          allItems={allClientes.length}
-          paginado={paginado}
-          />
-          <Button className='first-button mt-2' onClick={ e => {handleClick(e)} }> volver a cargar</Button>
-        </Col>
+  return (
+    <Container fluid className="px-4">
+      <Col sm={11}>
+        <h1>CLIENTES</h1>
+        <SearchBar inHome={true} inObrasSociales={false} />
+        <Row className="mt-4 mb-4">
+          <Col sm={12} className="d-flex justify-content-between align-items-center flex-wrap">
+            <Link to='/cliente'><Button className='first-button'>crear cliente</Button></Link> {' '}
+
+            <Dropdown isOpen={dropdownOpen} toggle={toggleDropdown} className="d-inline-block">
+              <DropdownToggle caret className="first-button mt-2"> ordenar </DropdownToggle>
+              <DropdownMenu>
+                <DropdownItem onClick={() => handleSort({ target: { value: 'asc' } })}>
+                  Ascendente
+                </DropdownItem>
+                <DropdownItem onClick={() => handleSort({ target: { value: 'desc' } })}>
+                  Descendente
+                </DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+
+            <Paginado
+              itemsPorPagina={clientesPorPagina}
+              allItems={allClientes.length}
+              paginado={paginado}
+            />
+            <Button className='first-button mt-2' onClick={e => { handleClick(e) }}> volver a cargar</Button>
+          </Col>
         </Row>
-        
+
         {
-          error ? 
-          
-          <Alert color="danger">{error}</Alert>
-          :
-          (
-          <div className="table-responsive">
-          <Table bordered className="custom-table shadow">
-            <thead>
-              <tr>
-                <th className="text-secondary fw-semibold">NOMBRE</th>
-                <th className="text-secondary fw-semibold">APELLIDO</th>
-                <th className="text-secondary fw-semibold">PUNTOS</th>
-                <th className="text-secondary fw-semibold">D.N.I</th>
-                <th className="text-secondary fw-semibold">OBRA SOCIAL</th>
-                <th className="text-secondary fw-semibold">AFILIADO</th>
-                <th className="text-secondary fw-semibold">DIRECCION</th>
-                <th className="text-secondary fw-semibold">TELEFONO</th>
-                <th className="text-secondary fw-semibold">NOTAS</th>
-                <th className="text-secondary fw-semibold">ACCIONES</th>
-              </tr>
-            </thead>
-            <tbody>
-              {currentClientes?.map((el) => (
-                <tr key={el.id}>
-                  <td className="fw-semibold">{el.nombre}</td>
-                  <td className="fw-semibold">{el.apellido}</td>
-                  <td className="points fw-semibold">{el.puntos}</td>
-                  <td className="text-secondary">{el.dni}</td>
-                  <td className="text-secondary">{el.obraSocials?.map((os, index) => (
-                      <div key={index}>{os.nombre.toUpperCase()}</div>
-                      ))}
-                  </td>
-                  <td className="text-secondary">{el.numeroDeAfiliado}</td>
-                  <td className="text-secondary">{el.direccion}</td>
-                  <td className="text-secondary">{el.telefono}</td>
-                  <td className="text-secondary">{el.notas}</td>
-                  <td>
-                    <Link to={`/cliente/${el.id}`}>
-                      <Button size="sm" className="mb-2 me-2 third-button">editar</Button>
-                    </Link>
-                    <Button size="sm" className=" mb-2 me-2 second-button" onClick={() => handleDelete(el.id)}>eliminar</Button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </Table>
-          </div>)
+          error ?
+
+            <Alert color="danger">{error}</Alert>
+            :
+            (
+              <div className="table-responsive">
+                <Table bordered className="custom-table shadow">
+                  <thead>
+                    <tr>
+                      <th className="text-secondary fw-semibold">NOMBRE</th>
+                      <th className="text-secondary fw-semibold">APELLIDO</th>
+                      <th className="text-secondary fw-semibold">PUNTOS</th>
+                      <th className="text-secondary fw-semibold">D.N.I</th>
+                      <th className="text-secondary fw-semibold">O.S</th>
+                      <th className="text-secondary fw-semibold">AFILIADO</th>
+                      <th className="text-secondary fw-semibold">DIRECCION</th>
+                      <th className="text-secondary fw-semibold">TELEFONO</th>
+                      <th className="text-secondary fw-semibold">NOTAS</th>
+                      <th className="text-secondary fw-semibold">ACCIONES</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {currentClientes?.map((el) => (
+                      <tr key={el.id}>
+                        <td className="fw-semibold">{el.nombre}</td>
+                        <td className="fw-semibold">{el.apellido}</td>
+                        <td className="points fw-semibold">{el.puntos}</td>
+                        <td className="text-secondary">{el.dni}</td>
+                        <td className="text-secondary">{el.obraSocials?.map((os, index) => (
+                          <div key={index}>{os.nombre.toUpperCase()}</div>
+                        ))}
+                        </td>
+                        <td className="text-secondary">{el.numeroDeAfiliado}</td>
+                        <td className="text-secondary">{el.direccion}</td>
+                        <td className="text-secondary">{el.telefono}</td>
+                        <td className="text-secondary">
+                          <div
+                            style={{
+                              maxHeight: "80px",
+                              minWidth: "180px",
+                              maxWidth: "181px",
+                              overflowY: "auto",
+                              overflowX: "hidden",
+                              whiteSpace: "pre-wrap",
+                              wordBreak: "break-word",
+                              paddingRight: "5px"
+                            }}>
+                            {el.notas}
+                          </div>
+                        </td>
+                        <td>
+                          <Link to={`/cliente/${el.id}`}>
+                            <Button size="sm" className="mb-2 me-2 third-button action-btn">editar</Button>
+                          </Link>
+                          <Button size="sm" className="mb-2 me-2 second-button action-btn" onClick={() => handleDelete(el.id)}>eliminar</Button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </Table>
+              </div>)
         }
       </Col>
-      </Container>
+    </Container>
   )
 
 }
--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import './index.css';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import store from './redux/store.js';
 import axios from "axios";
 
 
-//axios.defaults.baseURL = 'http://localhost:3001';
-axios.defaults.baseURL = 'https://demofarmacia-backend-production.up.railway.app/'; 
+axios.defaults.baseURL = 'http://localhost:3001';
+//axios.defaults.baseURL = 'https://demofarmacia-backend-production.up.railway.app/'; 
 
 
 
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
-          <App />
+      <App />
     </React.StrictMode>
   </Provider>,
   document.getElementById("root")
